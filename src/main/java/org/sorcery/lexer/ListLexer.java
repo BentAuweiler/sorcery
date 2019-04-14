722f2bd8ad3ad29a1bd61ca6e33c3f9d7f06e455
Bent Auweiler <bent.auweiler@gmail.com>
2019-04-14 09:47:43 +0200

    Complete the lexer implementation

--- src/main/java/org/sorcery/lexer/ListLexer.java
+++ src/main/java/org/sorcery/lexer/ListLexer.java
@@ -4,15 +4,15 @@ public class ListLexer extends Lexer {
 
     public static int NAME = 2;
     public static int COMMA = 3;
-    public static int LBRACK = 4;
-    public static int RBRACK = 5;
+    public static int LEFTBRACKET = 4;
+    public static int RIGTHBRACKET = 5;
     public static String[] tokenNames = {
         "n/a",
         "<EOF>",
         "NAME",
         "COMMA",
-        "LBRACK",
-        "RBRACK",
+        "LEFTBRACKET",
+        "RIGTHBRACKET",
     };
 
     public String getTokenName(int x){
@@ -24,6 +24,48 @@ public class ListLexer extends Lexer {
     }
 
     public boolean isLetter(){
-        return current_charackter >= 'a' && current_charackter <= 'z' || current_charackter >= 'A' && current_charackter <= 'Z';
+        return this.current_charackter >= 'a' && this.current_charackter <= 'z' || this.current_charackter >= 'A' && this.current_charackter <= 'Z';
+    }
+
+    public Token nextToken(){
+        while(this.current_charackter != EOF){
+            switch (this.current_charackter){
+                case ' ': case '\t': case '\n': case '\r': consumeWhiteSpaces(); continue;
+                case ',': consume(); return new Token(COMMA, ",");
+                case '[': consume(); return new Token(LEFTBRACKET, "[");
+                case ']': consume(); return new Token(RIGTHBRACKET, "]");
+                default:
+                    if(isLetter()){
+                        return getVariableToken();
+                    } else{
+                        throw new Error("invalid charackter: " + this.current_charackter);
+                    }
+
+            }
+        }
+        return new Token(EOF, "<EOF>");
+    }
+
+    private void consumeWhiteSpaces(){
+        while(isWhiteSpace()){
+            consume();
+        }
+    }
+
+    private boolean isWhiteSpace(){
+        if(this.current_charackter == ' ' || this.current_charackter == '\t' || this.current_charackter == '\n' || this.current_charackter == '\r'){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private Token getVariableToken(){
+        StringBuilder builder = new StringBuilder();
+        do {
+            builder.append(this.current_charackter);
+            consume();
+        } while(isLetter());
+        return new Token(NAME, builder.toString());
     }
 }
