834e56b59bf45459f514493236743764f6d835ff
Bent Auweiler <bent.auweiler@gmail.com>
2019-04-22 07:23:20 +0200

    SORC-1: Extend Lexer for Integers and Floats

--- src/main/java/org/sorcery/lexer/ListLexer.java
+++ src/main/java/org/sorcery/lexer/ListLexer.java
@@ -6,6 +6,8 @@ public class ListLexer extends Lexer {
     public static int COMMA = 3;
     public static int LEFTBRACKET = 4;
     public static int RIGTHBRACKET = 5;
+    public static int INTEGER = 6;
+    public static int FLOAT = 7;
     public static String[] tokenNames = {
         "n/a",
         "<EOF>",
@@ -13,6 +15,8 @@ public class ListLexer extends Lexer {
         "COMMA",
         "LEFTBRACKET",
         "RIGTHBRACKET",
+        "INTEGER",
+        "FLOAT",
     };
 
     public String getTokenName(int x){
@@ -21,10 +25,6 @@ public class ListLexer extends Lexer {
 
     public ListLexer(String input){
         super(input);
-    }
-
-    public boolean isLetter(){
-        return this.current_charackter >= 'a' && this.current_charackter <= 'z' || this.current_charackter >= 'A' && this.current_charackter <= 'Z';
     }
 
     public Token nextToken(){
@@ -37,13 +37,27 @@ public class ListLexer extends Lexer {
                 default:
                     if(isLetter()){
                         return getVariableToken();
-                    } else{
+                    } else if(isNumber()){
+                        return getNumberToken();
+                    }else{
                         throw new Error("invalid charackter: " + this.current_charackter);
                     }
 
             }
         }
         return new Token(EOF, "<EOF>");
+    }
+
+    public boolean isLetter(){
+        return this.current_charackter >= 'a' && this.current_charackter <= 'z' || this.current_charackter >= 'A' && this.current_charackter <= 'Z';
+    }
+
+    public boolean isNumber(){
+        return this.current_charackter >= '0' && this.current_charackter <= '9';
+    }
+
+    public boolean isDot(){
+        return this.current_charackter == '.';
     }
 
     private void consumeWhiteSpaces(){
@@ -58,6 +72,29 @@ public class ListLexer extends Lexer {
         } else {
             return false;
         }
+    }
+
+    private Token getNumberToken(){
+        StringBuilder builder = new StringBuilder();
+        boolean foundDot = false;
+        int numberOfDots = 0;
+        do {
+            if(this.current_charackter == '.'){
+                foundDot = true;
+                numberOfDots++;
+            }
+            if(numberOfDots > 1){
+                throw new Error("invalid charackter: " + this.current_charackter);
+            }
+            builder.append(this.current_charackter);
+            consume();
+        } while(isNumber() || isDot());
+        if(foundDot){
+            return new Token(FLOAT, builder.toString());
+        }else{
+            return new Token(INTEGER, builder.toString());
+        }
+        
     }
 
     private Token getVariableToken(){
