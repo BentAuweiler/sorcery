8a9c328dcd49b3ba96510daa9e0a0b08ce593c4d
Bent Auweiler <bent.auweiler@gmail.com>
2019-04-25 07:45:31 +0200

    SORC-2: Extend parser with backtracking ability

---
+++ src/main/java/org/sorcery/parser/MismatchedTokenException.java
@@ -0,0 +1,9 @@
+package org.sorcery.parser;
+
+class MismatchedTokenException extends Exception{
+
+    public MismatchedTokenException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/org/sorcery/parser/Parser.java
+++ src/main/java/org/sorcery/parser/Parser.java
@@ -1,23 +1,78 @@
 package org.sorcery.parser;
 
+import java.util.List;
+
 import org.sorcery.lexer.*;
 
 public abstract class Parser{
 
     private Lexer lexer;
-    protected Token lookahead;
+    private List<Integer> markers;
+    protected List<Token> lookahead;
+    private int pointer;
 
     public Parser(Lexer lexer){
         this.lexer = lexer;
     }
 
-    public void match(int x){
-        if(this.lookahead.type == x){
+    public void match(int x) throws MismatchedTokenException {
+        if( getTokenTypeFromLookahead(1) == x ){
             consume();
+        }else{
+            throw new MismatchedTokenException("expecting " + lexer.getTokenName(x) + " found " + getTokenAtLookaheadPosition(1));
         }
     }
 
     public void consume(){
-        this.lookahead = lexer.nextToken();
+        this.pointer++;
+        if( this.pointer == this.lookahead.size() && !isSpeculating() ){
+            this.pointer = 0;
+            this.lookahead.clear();
+        }
+        sync(1);
+    }
+
+    public Token getTokenAtLookaheadPosition(int position){
+        sync(position);
+        int currentPosition = this.pointer+position-1;
+        return this.lookahead.get( currentPosition );
+    }
+
+    public int getTokenTypeFromLookahead(int i){
+        return getTokenAtLookaheadPosition(i).type;
+    }
+
+    public void sync(int i){
+        int currentPosition = this.pointer+i-1;
+        int numberOfTokens = this.lookahead.size()-1;
+        if(currentPosition > numberOfTokens){
+            int n = currentPosition - numberOfTokens;
+            fill(n);
+        }
+    }
+
+    public void fill(int number){
+        for(int i = 1; i <= number; i++ ){
+            lookahead.add(lexer.nextToken());
+        }
+    }
+
+    public boolean isSpeculating(){
+        return this.markers.size() > 0;
+    }
+
+    public int mark(){
+        this.markers.add(this.pointer);
+        return this.pointer;
+    }
+
+    public void release(){
+        int marker = this.markers.size() - 1 ;
+        this.markers.remove(this.markers.size() - 1);
+        setPointer(marker);
+    }
+
+    public void setPointer(int index){
+        this.pointer = index;
     }
 }
